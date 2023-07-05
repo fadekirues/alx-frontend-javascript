d805962dec02d5891d7af9b5afed63485176caec
fadekirues <fdkiruegbukpe@gmail.com>
2023-07-05 23:06:08 +0000

    all my data manipulations

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,2 +1,6 @@
-export default  function getListStudentIds(){
+export default function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map((items) => items.id);
+  }
+  return [];
 }
